fb7ae946a68697605faf2ae2d7d72115011ba9fc
Makotosoul <minhthien.ng4612@gmail.com>
2024-04-26 19:25:38 +0300

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-
+import {
+	BrowserRouter,
+	Route,
+	Routes,
+	Link,
+} from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Drawer from "@mui/material/Drawer";
+import ListItemButton from "@mui/material/ListItemButton";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
@@ -12,55 +21,68 @@ import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import CustomerList from "./components/Customer/CustomerList";
 import TrainingList from "./components/Training/TrainingList";
 import Calendar from "./components/Training/Calendar";
-function App() {
-	const [value, setValue] = React.useState("customerList");
 
-	const handleChange = (event, newValue) => {
-		setValue[newValue];
+function App() {
+
+	const [state, setState] = React.useState({
+		left: false,
+	});
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+		setState({ ...state, [anchor]: open });
 	};
+
+	const list = () => (
+		<List>
+			<ListItemButton component={Link} to="/">
+				<PeopleAltIcon />
+				<ListItemText primary="Customer List" />
+			</ListItemButton>
+			<ListItemButton component={Link} to="/training">
+				<FitnessCenterIcon />
+				<ListItemText primary="Training List" />
+			</ListItemButton>
+			<ListItemButton component={Link} to="/calendar">
+				<CalendarMonthIcon />
+				<ListItemText primary="Calendar" />
+			</ListItemButton>
+		</List>
+	);
 	return (
 		<BrowserRouter>
 			<AppBar position="static">
-				<Box sx={{ width: "100%" }}>
-					<Tabs
-						value={value}
-						onChange={handleChange}
-						textColor="inherit"
-						indicatorColor="inherit"
-					>
-						<Tab
-							value="customerList"
-							label={
-								<>
-									<PeopleAltIcon /> Customer List
-								</>
-							}
-							component={Link}
-							to="/"
-						/>
-						<Tab
-							value="trainingList"
-							label={
-								<>
-									<FitnessCenterIcon /> Training List
-								</>
-							}
-							component={Link}
-							to="/training"
-						/>
-						<Tab
-							value="calendar"
-							label={
-								<>
-									<CalendarMonthIcon />
-									Calendar
-								</>
-							}
-							component={Link}
-							to="/calendar"
-						/>
-					</Tabs>
-				</Box>
+				<Toolbar>
+					{["left"].map((anchor) => (
+						<React.Fragment key={anchor}>
+							<IconButton
+								edge="start"
+								color="inherit"
+								aria-label="menu"
+								onClick={toggleDrawer(anchor, true)}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Drawer
+								anchor="left"
+								open={state["left"]}
+								onClose={toggleDrawer("left", false)}
+							>
+								{list()}
+							</Drawer>
+						</React.Fragment>
+					))}
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ flexGrow: 1 }}
+					>Personal Trainer</Typography>
+				</Toolbar>
 			</AppBar>
 			<Routes>
 				<Route path="/" exact Component={CustomerList} />
